03ba8ebb0c876ca27461c3a962b4eb1efa0d54fc
Fernando L. Canizo <flc@muriandre.com>
2015-06-04 08:39:37 -0300

    Implement JSON request on server
    Package follow-redirects was needed as the test URL does a 301 redirect.

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,28 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Team42 Monitoring' });
 });
 
+
+router.get('/metrics', function(expressRequest, expressResponse, next) {
+	var http = require('follow-redirects').http;
+	var url = "http://52.24.23.194/metrics";
+
+	http.get(url, function(httpResponse) {
+		var data = '';
+
+		httpResponse.on('data', function (chunk) {
+			data += chunk;
+		});
+
+		httpResponse.on('end', function () {
+			expressResponse.json(data);
+		});
+
+	})
+	.on('error', function(e) {
+		console.log("Error when calling external metrics site %s: %s", url, e.message);
+		expressResponse.json({status: false, statusMessage: e.message});
+	});
+});
+
+
 module.exports = router;
